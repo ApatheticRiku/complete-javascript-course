dd1af3646484d2fcd81f89fb11478101fc94150f
Tony DeMarco <tony@limepine.net>
2024-01-26 12:05:30 -0500

    11-Data_Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,14 @@
 let country = "United States of America";
 let continent = "North America";
 let population = 341034672;
+let isIsland = false;
+let language;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
