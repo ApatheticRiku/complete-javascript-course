de6988f930aec999a6a2782fc7b558984117fc40
Tony DeMarco <tony@limepine.net>
2024-01-27 16:22:51 -0500

    44-Object_methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -103,7 +103,6 @@ console.log(friends.includes(23));
 if (friends.includes("Steven")) {
   console.log("You have a friend call Steven!");
 }
-*/
 
 const jonas = {
   firstName: "Jonas",
@@ -142,3 +141,40 @@ console.log(jonas);
 console.log(
   `${jonas.firstName} has ${jonas.friends.length}, and his best friend is called ${jonas.friends[0]}`
 );
+*/
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // },
+
+  // calcAge: function () {
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he has driver's ${this.hasDriversLicense ? "a" : "no"} license`;
+  },
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+console.log(jonas.getSummary());
