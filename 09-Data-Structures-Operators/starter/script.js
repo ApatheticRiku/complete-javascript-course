3f937f112ba9b50b255499e3c32437d2cfa764dd
Tony DeMarco <tony@limepine.net>
2024-01-28 23:12:12 -0500

    111-Coding_Challenge_1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,7 @@ const restaurant = {
   },
 };
 
+/*
 const rest1 = {
   name: "Capri",
   // numGuests: 20,
@@ -84,7 +85,6 @@ rest2.owner &&= "<ANONYMOUS";
 console.log(rest1);
 console.log(rest2);
 
-/*
 // restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -233,4 +233,76 @@ console.log(main, secondary);
 
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
+*/
+/*
+/////////////////////////////////////////////
+// Coding Challenge #1
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Periscic"];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals("Davies", "Miller", "Lewandowski", "Kimmich");
+printGoals("Davies", "Miller");
+printGoals(...game.scored);
+
+team1 < team2 && console.log("Team 1 is more likely to win");
+team2 < team1 && console.log("Team 2 is more likely to win");
 */
